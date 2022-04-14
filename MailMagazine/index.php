dbbe9a7db526e858a838f28e28d5616a5d9c2af5
susitaro3 <70739647+susitaro3@users.noreply.github.com>
2022-04-14 11:18:22 +0900

    Add files via upload

---
+++ MailMagazine/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+<head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HS6V59RMF6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-HS6V59RMF6');
+
+  gtag('event_name', 'MM0414');
+
+</script>
+</head>
+</html>
